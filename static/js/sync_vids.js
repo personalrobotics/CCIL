d84ff72952ac54c1a897cfd58f67595f64cd7288
Abhay D <a.deshpande012@gmail.com>
2024-04-19 01:31:32 -0700

    Update videos, improve video synchronization

--- static/js/sync_vids.js
+++ static/js/sync_vids.js
@@ -1,13 +1,27 @@
 $(document).ready(function() {
     const syncedVids = document.querySelectorAll('video.synced');
 
+    let vidsReady = 0;
     let vidsEnded = 0;
 
     for (const vid of syncedVids) {
+        vid.oncanplay = () => {
+            vid.oncanplay = null;
+            vidsReady += 1;
+            console.log(`${vidsReady}/${syncedVids.length} videos ready`);
+            if (vidsReady === syncedVids.length) {
+                console.log("Starting all videos");
+                for (const v of syncedVids) {
+                    v.play();
+                }
+            }
+        }
+
         vid.onended = () => {
             vidsEnded += 1;
-            console.log(vidsEnded);
+            console.log(`${vidsEnded}/${syncedVids.length} videos finished`);
             if (vidsEnded === syncedVids.length) {
+                console.log("Restarting all videos");
                 setTimeout(() => {
                     vidsEnded = 0;
                     for (const v of syncedVids) {
